1df4ac5722c75fa6794d9f5aaa770b6f7c8e4399
Ian Collins <ian.collins@gmail.com>
2009-08-19 16:14:50 -0700

    new unittestframework base code and configuration

--- Tests/config.js
+++ Tests/config.js
@@ -1,11 +1,16 @@
-UnitTester.site  = 'Mooish Sample';
-UnitTester.title = 'Interactive Tests';
-
-window.addEvent('load', function(){
-	new UnitTester({
-		mootoolsCore : '../../mootools-core',
-		Sample			 : '..'
-	},{
-		Sample : 'UserTests'
-	});
-});
+var UnitTester = {
+	site: 'Mooish Sample',
+	title: 'Unit Tests',
+	path: 'UnitTester/',
+	ready: function(){
+		var sources = {
+			mootoolsCore : '../../mootools-core',
+			Sample			 : '..'
+		};
+		new UnitTester(sources, {
+			Sample : 'UserTests'
+		}, {
+			autoplay: true
+		});
+	}
+};
